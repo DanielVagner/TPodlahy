aa0b9a6123ee277a2b6a37b780f4abdca58d969e
Daniel Vagner <daniel.vagner@certicon.cz>
2024-03-09 22:02:36 +0100

    New images and change in home page

--- src/app/shared/process/process.component.ts
+++ src/app/shared/process/process.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-process',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./process.component.scss']
 })
 export class ProcessComponent {
+
+  safeURL: SafeResourceUrl;
+
+  constructor(private _sanitizer: DomSanitizer){
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/watch?v=5oJP9X16etg&t');
+ }
 
 }
