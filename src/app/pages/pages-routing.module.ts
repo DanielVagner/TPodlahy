d741a54483fa94d0d5030bd4d9f318686afc0b8b
Daniel Vagner <daniel.vagner@certicon.cz>
2024-03-09 20:23:22 +0100

    first commit

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Component
+import { IndexComponent } from './index/index.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:IndexComponent
+  },
+  {
+    path:'index1',
+    component:IndexComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
